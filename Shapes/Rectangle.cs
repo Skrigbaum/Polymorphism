0fbded010e967cff347527a47e3b54745a46dd39
Spencer Krigbaum <skrigbaum872002@gmail.com>
2015-10-26 11:56:50 -0400

    Added shape classes

---
+++ Shapes/Rectangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    class Rectangle : Shape
+    {
+        private double length;
+        private double width;
+
+        public double Length
+        {
+            get
+            {
+                return length;
+            }
+
+            set
+            {
+                length = value;
+            }
+        }
+
+        public double Width
+        {
+            get
+            {
+                return width;
+            }
+
+            set
+            {
+                width = value;
+            }        
+        }
+       public Rectangle(double height, double width)
+        {
+            double Width = width;
+            double Height = height;
+        }
+        }
+}
